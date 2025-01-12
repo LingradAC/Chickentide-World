6791b0cd54feaf7c37889d6b16ac56465b6a98f8
Aaron Baerbock <152320820+LingradAC@users.noreply.github.com>
2025-01-12 17:38:04 -0600

    Update RebaseWorldBase.bat

--- Database/compiled/RebaseWorldBase.bat
+++ Database/compiled/RebaseWorldBase.bat
@@ -5,10 +5,10 @@ set /p password=<password.txt
 REM execute Update Scripts for World Database
 For /R "..\2-BaseUpdates\" %%G IN (*.sql) do (
 echo Found file: %%G
-"C:\Program Files\MySql\MySQL Server 8.0\bin\mysql.exe" -h localhost -u root -p%password% ace_world < %%G
+"C:\Program Files\MySql\MySQL Server 8.0\bin\mysql.exe" -h localhost -u root -p%password% ctd_world < %%G
 )
 
 del ..\1-Base\WorldBase.sql
 del ..\2-BaseUpdates\*.sql
 
-"C:\Program Files\MySql\MySQL Server 8.0\bin\mysqldump.exe" --user=root --password=%password% --databases ace_world --add-drop-database --add-drop-table --create-options --quote-names --lock-tables --dump-date --flush-privileges --set-gtid-purged=AUTO --disable-keys --tz-utc --add-locks --extended-insert --opt --no-data | sed\sed "s/ AUTO_INCREMENT=[0-9]*\b//g" > ..\1-Base\WorldBase.sql
+"C:\Program Files\MySql\MySQL Server 8.0\bin\mysqldump.exe" --user=root --password=%password% --databases ctd_world --add-drop-database --add-drop-table --create-options --quote-names --lock-tables --dump-date --flush-privileges --set-gtid-purged=AUTO --disable-keys --tz-utc --add-locks --extended-insert --opt --no-data | sed\sed "s/ AUTO_INCREMENT=[0-9]*\b//g" > ..\1-Base\WorldBase.sql
