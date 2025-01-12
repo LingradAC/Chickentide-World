522bbde7ad9b87bc4b741da10e7e755690d943a9
Aaron Baerbock <152320820+LingradAC@users.noreply.github.com>
2025-01-12 17:35:18 -0600

    Update CreatePreRelease.bat

--- Database/compiled/CreatePreRelease.bat
+++ Database/compiled/CreatePreRelease.bat
@@ -1,8 +1,8 @@
 set /p password=<password.txt
-del 16PY-db.sql
-"C:\Program Files\MySql\MySQL Server 8.0\bin\mysqldump.exe" --user=root --password=%password% --databases ace_world --add-drop-database --add-drop-table --create-options --quote-names --lock-tables --dump-date --flush-privileges --set-gtid-purged=AUTO --disable-keys --tz-utc --add-locks --extended-insert --opt --skip-triggers > 16PY-db.sql
+del ctd-db.sql
+"C:\Program Files\MySql\MySQL Server 8.0\bin\mysqldump.exe" --user=root --password=%password% --databases ctd_world --add-drop-database --add-drop-table --create-options --quote-names --lock-tables --dump-date --flush-privileges --set-gtid-purged=AUTO --disable-keys --tz-utc --add-locks --extended-insert --opt --skip-triggers > ctd-db.sql
 @echo off
-echo. >> 16PY-db.sql
-copy /b 16PY-db.sql + ..\1-Base\WorldTriggers.sql 16PY-db.sql
-echo. >> 16PY-db.sql
+echo. >> ctd-db.sql
+copy /b ctd-db.sql + ..\1-Base\WorldTriggers.sql ctd-db.sql
+echo. >> ctd-db.sql
 @echo on
